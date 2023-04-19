29cb0efd866654725dadcd5ceed119cd349a8526
ridzRUSH <12adarshpandey12@gmail.com>
2023-04-19 07:54:59 -0400

    complete belman ford algo

--- BelmanFordAlgorithm.cpp
+++ BelmanFordAlgorithm.cpp
@@ -5,9 +5,35 @@ using namespace std;
 
 int main(){
 	// assuming threre is edges comming with edges and weight of edges
-	
-	v = 10;
+	// V is no of nodes
+
+	int V = 10;
+	// mark every one inf
 	std::vector<int> dist(v,1e8);
+
+	for(int i=0 ; i< V -1 ;i++ ){
+		for(auto it : edges){
+			int u = it[0];
+			int v = it[1];
+			int ew = it[2];
+			if(dist[u]!=1e8 && dist[u]+ ew < dist[v]){
+				dist[v]= dist[u]+ew;
+			}
+		}
+	}
+	// to dedtect the negative cycle
+	for(auto it : edges){
+			int u = it[0];
+			int v = it[1];
+			int ew = it[2];
+			if(dist[u]!=1e8 && dist[u]+ ew < dist[v]){
+				cout<<"negative cycle"<<endl;
+				break;
+			}
+	}
+
+	// print dist if no egative cycle
+
 
 
 	
